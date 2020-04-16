d3674ebd489e43eec29e70f63383ca0bad558892
Frode <frode@fintlabs.no>
2020-04-16 11:33:08 +0200

    small changes

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const figlet = require('figlet');
 const utils = require('./lib/utils');
 const arm = require('./lib/arm');
 const project = require('./lib/project');
-
 const yargs = require("yargs");
 
 
@@ -24,22 +23,23 @@ console.log(
 
 yargs.scriptName('yaiact')
     .usage('$0 <cmd> [args]')
-    .command('init [name]', 'Create a new project', (yargs) => {
+    .command('init <name>', 'Create a new project', (yargs) => {
         yargs
-        .positional('name', {
-            type: 'string',
-            require: true,
-            describe: 'name of project'
-        })
+            .positional('name', {
+                type: 'string',
+                require: true,
+                describe: 'name of project'
+            })
     }, (argv) => {
-       project.init(argv.name); 
+        project.init(argv.name);
     })
-    .command('clean', 'Clean build directory', (yargs) => {
+    .command('clean', 'Clean output directory', (yargs) => {
         yargs.options('d', {
             alias: 'directory',
             type: 'string',
             default: 'output',
-            describe: 'name of the build directory'
+            describe: 'name of the output directory',
+            hidden: true
         })
     }, (argv) => {
         utils.cleanBuildDirectory(argv.d);
@@ -49,15 +49,17 @@ yargs.scriptName('yaiact')
             alias: 'directory',
             type: 'string',
             default: 'output',
-            describe: 'name of the build directory'
+            describe: 'name of the build directory',
+            hidden: true
         })
     }, (argv) => {
         arm.generateArm(argv.d);
+        arm.generateDeployScript(argv.d);
     })
     .command('update variables', 'Update variables', (yargs) => {}, (argv) => {
         arm.updateVariables();
     })
-    .command('new rg [name] [subscription]', 'Generate skeleton for resource group', (yargs) => {
+    .command('new rg <name> <subscription> <location>', 'Generate skeleton for resource group', (yargs) => {
         yargs
             .positional('name', {
                 type: 'string',
@@ -69,12 +71,18 @@ yargs.scriptName('yaiact')
                 require: true,
                 describe: 'name of subscription'
             })
+            .positional('location', {
+                type: 'string',
+                require: true,
+                describe: 'location'
+            })
     }, (argv) => {
         if (!argv.name || !argv.subscription) {
             yargs.showHelp();
             process.exit(0);
         }
-        arm.createResourceGroupSkeleton(argv.subscription, argv.name);
+        arm.createResourceGroupSkeleton(argv.subscription, argv.name, argv.location);
     })
+    .demandCommand(1, 'You need at least one command before moving on!')
     .help()
     .argv;
